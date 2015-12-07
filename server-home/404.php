230c5013e9904e7b8045bc984dba2714d9b9e55c
Chengming Liao <liaocm@berkeley.edu>
2015-12-06 23:23:10 -0800

    404

---
+++ server-home/404.php
@@ -0,0 +1,12 @@
+<?php
+http_response_code(404);
+echo '<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<HTML><HEAD>
+<TITLE>404 Not Found</TITLE>
+</HEAD><BODY>
+<H1>Not Found</H1>
+The requested URL was not found on this server.
+<HR>
+<I></I>
+</BODY></HTML>';
+?>
